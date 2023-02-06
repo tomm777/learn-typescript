6bc158d9bdc103cc30324adecd1a8fc0c86ea83b
tomm777 <tbbk1404@gmail.com>
2023-02-06 21:35:22 +0900

    add

--- quiz/1_todo/src/index.ts
+++ quiz/1_todo/src/index.ts
@@ -1,3 +1,19 @@
+/**
+ * a.js 내용
+ */
+// let arr = [
+//   {gender: "male", name : 'john'},
+//   {gender: "female", name : 'min',}
+// ]
+// let filtered = arr.filter(function(item) {
+//   if (item.gender === "male"){
+//       return item
+//   }
+// })
+// console.log(filtered);
+
+/***********************************/
+
 // type Todo = {
 //   id: number;
 //   title: string;
